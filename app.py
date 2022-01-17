e3495eefef8016cde43b72f9ff41820cf5937e5a
Geza Denes <denesgeza@gmail.com>
2022-01-17 22:07:51 +0200

    added methods to login route

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
+from email_validator import ValidatedEmail
 
 
 app = Flask(__name__)
@@ -16,21 +17,30 @@ class LoginForm(FlaskForm):
     remember = BooleanField('remember me')
 
 
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
     return render_template('login.html', form=form)
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
-    return render_template('signup.html')
+    form = RegisterForm()
+
+    return render_template('signup.html', form=form)
 
 
 @app.route('/dashboard')
