94378436308a5da75e4d2ac49402af237e3c0469
Geza Denes <denesgeza@gmail.com>
2022-01-18 15:55:24 +0200

    finish signup route connected with db

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
+''' Video from
+https://www.youtube.com/watch?v=8aTnmsDMldY
+'''
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
-from email_validator import ValidatedEmail
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, String, DateTime, Integer, create_engine
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisthesecretkey'
+conn = 'sqlite:////Users/denesgeza/Dropbox/Python/Projects/Flask/database.db'
+engine = create_engine(conn, echo=True)
 Bootstrap(app)
+Session = sessionmaker()
+Base = declarative_base()
+local_session = Session(bind=engine)
+
+
+class User(Base):
+
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(15), unique=True)
+    email = Column(String(50), unique=True)
+    password = Column(String(80), unique=True)
 
 
 class LoginForm(FlaskForm):
@@ -32,14 +50,22 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+        user = local_session.query(User).filter(User.username == form.username.data).first()
+        if user:
+            if user.password == form.password.data:
+                return redirect(url_for('dashboard'))
+        return '<h1>Invalid username or password</h1>'
     return render_template('login.html', form=form)
 
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     form = RegisterForm()
-
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        local_session.add(new_user)
+        local_session.commit()
+        return '<h1> New user has been created! </h1>'
     return render_template('signup.html', form=form)
 
 
